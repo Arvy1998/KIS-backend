78e7e5c3323a7709308d7ad8d565a0406500b575
Arvydas Baranauskas <arvydas.baranauskas@hashtages.com>
2020-12-21 21:23:45 +0200

    fix

--- src/controllers/user/get.js
+++ src/controllers/user/get.js
@@ -2,6 +2,7 @@ import User from 'models/User';
 
 const getUser = async (email) => {
   const user = await User.findOne({ email });
+  console.log({user});
   if (student) {
     return {
       _id: user._id,
